a733cf68a71b1f98e739723b04fd4ef4e8f51e7a
TheCodeFather0 <raminmammadov056@gmail.com>
2025-02-14 18:46:21 +0400

    l-17 added

---
+++ Lesson 18/app.js
@@ -0,0 +1,55 @@
+// let username = "insun";
+// console.log(username);
+
+// username = "dev";
+
+// let mesaj = "Hi! I am " + username;
+// console.log(mesaj);
+
+// let ad_soyad = "Murad Insun";
+// const DOGUM_ILI = "30.04.2003";
+
+// let mesaj = "salam! Men " + ad_soyad + ".Men " + DOGUM_ILI + " dogumluyam";
+// console.log(mesaj);
+
+// alert
+// prompt
+// confirm
+
+// alert("salam flankes!");
+
+// const ad = prompt("adin nedir?");
+// const kursa_gelessen = confirm("gelessen yetim?");
+// console.log(kursa_gelessen);
+
+// const a = prompt("birinci reqemi daxil et");
+// const b = prompt("ikinci reqemi daxil et");
+
+// console.log(Number(a) + Number(b));
+// NaN -> Not a Number
+
+// riyazi operatorlar
+
+// const a = 10;
+// const b = 3;
+// const cem = a + b;
+// const hasil = a * b;
+// const qismet = a / b;
+// const quvvet = a ** b;
+// const qaliq = a % b;
+// const ferq = a - b;
+
+// console.log(ferq);
+// <= >=
+
+// = -> assignment
+// == -> equality
+// === -> strict equality
+
+/*
+    +
+    -
+    *
+    /
+    **
+*/
